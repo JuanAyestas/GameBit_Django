e34809ab9ec95796012a369ffbb72d7edcfed09a
Juan Ayestas <juanca.ayestas0500@gmail.com>
2020-06-17 17:30:00 -0600

    updated settings

--- project_bit/settings.py
+++ project_bit/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = os.environ.get("DEBUG_VALUE")
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["gamebit-council.herokuapp.com"]
 
 # Application definition
 
