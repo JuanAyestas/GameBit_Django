21c3f2f7ea7add69b9c04b620fb0765bf389b596
Juan Ayestas <juanca.ayestas0500@gmail.com>
2020-06-18 04:23:10 -0600

    added url for error pages

---
+++ errors_bit/urls.py
@@ -0,0 +1,6 @@
+from . import views 
+
+handler404 = 'errors_bit.views.error_404'
+handler500 = 'errors_bit.views.error_500'
+handler403 = 'errors_bit.views.error_403'
+handler400 = 'errors_bit.views.error_400'
--- errors_bit/views.py
+++ errors_bit/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def error_404(request, exception):
-    return render(request, 'errors_bit/404.html')
+    return render(request, 'errors_bit/404.html', status=404)
 
 
 def error_403(request, exception):
-    return render(request, 'errors_bit/403.html')
+    return render(request, 'errors_bit/403.html', status=403)
 
 
 def error_500(request,  exception):
-    return render(request, 'errors_bit/500.html')
+    return render(request, 'errors_bit/500.html', status=500)
