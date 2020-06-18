60b3fed006ae3469865bb20b269e65204f3621c7
Juan Ayestas <juanca.ayestas0500@gmail.com>
2020-06-18 02:34:17 -0600

    Set the debug value

--- project_bit/settings.py
+++ project_bit/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_DJANGO")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG_VALUE")
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ["gamebit-council.herokuapp.com", ""]
 
