f2f53b123cb9b9dec5bf8187855751218d2da649
Juan Ayestas <juanca.ayestas0500@gmail.com>
2020-06-22 23:00:52 -0600

    made the staff url shorter

--- users_bit/urls.py
+++ users_bit/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name="gamebit-register"),
-    path('register/staff/df28fa638d23be003d72cc6330d03efb55301522f521437879e2276c8c4837c1a365236998703b1bf6b8c25c7c9a75ad06f549fb9d47da67bfc40dad1d5693079d6ad9c4571617b9964cec83494a4861f2a874dc74c717c53dc9c7522a171ca02d69817e6e8135beca02cc935e68d8a1addbeeb437da0b49d673b0fcd3499f385c311eba9e53bcc85893d344cc01d0936545dc2b7805f59613be80', views.staff_register, name="gamebit-staff"),
+    path('register/staff/Bj_hubwWcHH9DsnAxQCuGrDLfjEt79dFVwKzQTxMdk4',views.staff_register, name="gamebit-staff"),
     path("login/", auth_views.LoginView.as_view(template_name="users_bit/login.html"), name="gamebit-login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users_bit/logout.html"),name="gamebit-logout"),
     path("password-reset/", 
